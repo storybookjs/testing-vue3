ec55251c1b1ef2c8662b6fe8bc5f12697e67aa95
Yann Braga <yannbf@gmail.com>
2021-05-25 23:27:11 +0200

    refactor: move types to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,8 @@
-import type { ComponentOptions, ConcreteComponent } from "vue";
 import { combineParameters } from "@storybook/client-api";
-import {
-  ArgTypes,
-  Parameters as SbParameters,
-  BaseDecorators,
-} from "@storybook/addons";
-import { Story, Meta, StoryContext } from "@storybook/vue3";
+import type { Story, Meta, StoryContext } from "@storybook/vue3";
+import type { StoryFnVueReturnType, ContextedStory, GlobalConfig, StoriesWithPartialProps } from "./types";
+
 import decorateStory from "./decorateStory";
-
-type StoryFnVueReturnType = string | ComponentOptions<any>;
-/**
- * Object representing the preview.ts module
- *
- * Used in storybook testing utilities.
- * @see [Unit testing with Storybook](https://storybook.js.org/docs/react/workflows/unit-testing)
- */
-export type GlobalConfig = {
-  decorators?: BaseDecorators<StoryFnVueReturnType>;
-  parameters?: SbParameters;
-  argTypes?: ArgTypes;
-  [key: string]: any;
-};
-
-type Head<T extends any[]> = T extends [...infer Head, any] ? Head : any[];
-
-/**
- * A StoryFn where the context is already curried
- * in other words no more context param at the end
- */
-type ContextedStory<GenericArgs> = (
-  ...params: Partial<Head<Parameters<Story<Partial<GenericArgs>>>>>
-) => ConcreteComponent;
-
-/**
- * T represents the whole es module of a stories file. K of T means named exports (basically the Story type)
- * 1. pick the keys K of T that have properties that are Story<AnyProps>
- * 2. infer the actual prop type for each Story
- * 3. reconstruct Story with Partial. Story<Props> -> Story<Partial<Props>>
- */
-export type StoriesWithPartialProps<T> = {
-  [K in keyof T as T[K] extends Story<any> ? K : never]: T[K] extends Story<
-    infer P
-  >
-    ? ContextedStory<P>
-    : unknown;
-};
 
 let globalStorybookConfig: GlobalConfig = {};
 
---
+++ src/types.ts
@@ -0,0 +1,45 @@
+import type { ComponentOptions, ConcreteComponent } from "vue";
+import type {
+  ArgTypes,
+  Parameters as SbParameters,
+  BaseDecorators,
+} from "@storybook/addons";
+import type { Story } from "@storybook/vue3";
+
+export type StoryFnVueReturnType = string | ComponentOptions<any>;
+/**
+ * Object representing the preview.ts module
+ *
+ * Used in storybook testing utilities.
+ * @see [Unit testing with Storybook](https://storybook.js.org/docs/react/workflows/unit-testing)
+ */
+export type GlobalConfig = {
+  decorators?: BaseDecorators<StoryFnVueReturnType>;
+  parameters?: SbParameters;
+  argTypes?: ArgTypes;
+  [key: string]: any;
+};
+
+export type Head<T extends any[]> = T extends [...infer Head, any] ? Head : any[];
+
+/**
+ * A StoryFn where the context is already curried
+ * in other words no more context param at the end
+ */
+export type ContextedStory<GenericArgs> = (
+  ...params: Partial<Head<Parameters<Story<Partial<GenericArgs>>>>>
+) => ConcreteComponent;
+
+/**
+ * T represents the whole es module of a stories file. K of T means named exports (basically the Story type)
+ * 1. pick the keys K of T that have properties that are Story<AnyProps>
+ * 2. infer the actual prop type for each Story
+ * 3. reconstruct Story with Partial. Story<Props> -> Story<Partial<Props>>
+ */
+export type StoriesWithPartialProps<T> = {
+  [K in keyof T as T[K] extends Story<any> ? K : never]: T[K] extends Story<
+    infer P
+  >
+    ? ContextedStory<P>
+    : unknown;
+};
